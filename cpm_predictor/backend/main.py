7159e9e601a8450b082cf81ab8b6fe221a957020
AdwitBarun <adwitb2003@kgpian.iitkgp.ac.in>
2025-12-20 01:14:16 +0530

    done

--- cpm_predictor/backend/main.py
+++ cpm_predictor/backend/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-from cpm_predictor.backend.app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="CPM Predictor API")
+from .app.routes import router   # ✅ relative import
+
+app = FastAPI(
+    title="CPM Prediction API",
+    version="2.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
